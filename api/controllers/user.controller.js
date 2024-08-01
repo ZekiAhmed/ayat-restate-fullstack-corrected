8690f7e76602546ffd9aabc08190ef997c980550
ZekiAhmed <changezworld3@gmail.com>
2024-07-31 20:07:17 -0700

    logic to update profile user name on navbar

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -46,7 +46,9 @@ export const updateUser = async (req, res) => {
         ...(avatar && { avatar }),
       },
     });
-    res.status(200).json(updatedUser);
+
+    const { password: userPassword, ...rest } = updatedUser;
+    res.status(200).json(rest);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to update user!" });
