a011b2340f05f4300e00e6567a3d54031c40beb0
ZekiAhmed <changezworld3@gmail.com>
2024-08-08 15:56:41 -0700

    editing MapContainer

--- client/src/components/map/Map.jsx
+++ client/src/components/map/Map.jsx
@@ -1,19 +1,28 @@
-import { MapContainer, TileLayer } from 'react-leaflet';
-import './map.scss';
-import 'leaflet/dist/leaflet.css'
-import Pin from '../pin/Pin';
-const Map = ({items}) => {
+import { MapContainer, TileLayer } from "react-leaflet";
+import "./map.scss";
+import "leaflet/dist/leaflet.css";
+import Pin from "../pin/Pin";
+const Map = ({ items }) => {
   return (
-    <MapContainer center={[52.4997, -1.90269]} zoom={7} scrollWheelZoom={true} className='map'>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    {items.map((item)=>(
-      <Pin key={item.id} item={item}/>
-    ))}
-  </MapContainer>
-  )
-}
+    <MapContainer
+      center={
+        items.length === 1
+          ? [items[0].latitude, items[0].longitude]
+          : [52.4997, -1.90269]
+      }
+      zoom={7}
+      scrollWheelZoom={true}
+      className="map"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {items.map((item) => (
+        <Pin key={item.id} item={item} />
+      ))}
+    </MapContainer>
+  );
+};
 
-export default Map
+export default Map;
